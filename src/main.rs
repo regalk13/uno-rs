7acc39606f4540c054f329a450c528df53b9f776
A.L.I.C.E <20363359+alice39@users.noreply.github.com>
2022-11-15 21:26:09 -0400

    Change src/main.rs to be a functional component

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,24 @@
 use yew::prelude::*;
 
-enum Msg {
-    AddOne,
-    Reset
-}
+#[function_component(Model)]
+fn model_functional() -> Html {
+    let value = use_state(|| 0i64);
+    let on_add = {
+        let value = value.clone();
+        Callback::from(move |_| value.set(*value + 1))
+    };
+    let on_reset = {
+        let value = value.clone();
+        Callback::from(move |_| value.set(0))
+    };
 
-struct Model {
-    value: i64,
-}
+    html! {
+        <div>
+            <button onclick={on_add}>{ "+1" }</button>
 
-impl Component for Model {
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {
-            value: 0,
-        }
-    }
-
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
-                true
-            }
-            Msg::Reset => {
-                self.value = 0;
-                true
-            }
-        }
-    }
-
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
-        let link = ctx.link();
-        html! {
-            <div>
-                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
-
-                <button onclick={link.callback(|_| Msg::Reset)}>{ "Reset" }</button>
-                <p>{ self.value }</p>
-            </div>
-        }
+            <button onclick={on_reset}>{ "Reset" }</button>
+            <p>{ *value }</p>
+        </div>
     }
 }
 
