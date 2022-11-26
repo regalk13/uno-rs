0624c6f71a7523db8b64dd58ebcf536ca31840d1
Regalk <jsalazarcaviedes30@gmail.com>
2022-11-26 12:26:15 -0500

    Add style to login view

--- uno-frontend/src/router/views/login.rs
+++ uno-frontend/src/router/views/login.rs
@@ -4,16 +4,28 @@ use yew::prelude::*;
 #[function_component(Login)]
 pub fn login() -> Html {
     html! {
-        <section>
-        <h1>{"Login"}</h1>
-        <form>
-            <label>{"Name"}</label>
-            <input type="text" name="name" placeholder="regalk"/>
-            <label>{"Password"}</label>
-            <input type="password" name="password" placeholder="password"/>
-
-            <input type="submit" name="login" value="login"/>
-        </form>
-        </section>
+    <section class="flex items-center justify-center min-h-screen bg-gray-300">
+        <div class="px-8 py-6 mt-4 text-left bg-white shadow-lg">
+            <h1 class="text-2x1 font-bold text-center">{"Login"}</h1>
+            <form action="">
+                    <div class="mt-4">
+                        <div>
+                            <label class="block">{"Username"}</label>
+                            <input class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-ringblue-600"
+                                   type="text" name="name" placeholder="Username"/>
+                        </div>
+                        <div class="mt-4">
+                            <label class="block">{"Password"}</label>
+                            <input class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-ringblue-600"
+                                   type="password" name="password" placeholder="password"/>
+                        </div>
+                         <div class="flex items-baseline justify-between">
+                            <input type="submit" class="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900" value="Login"/>
+                            <a href="#" class="text-sm text-blue-600 hover:underline">{"Forgot password?"}</a>
+                        </div>
+                    </div>
+            </form>
+        </div>
+    </section>
     }
 }
