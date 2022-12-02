a16fe524d1f647acd7eeea9b6beeb65c04af4c4d
Regalk <jsalazarcaviedes30@gmail.com>
2022-12-02 08:22:24 -0500

    Add comments and HTML tags for good practices

--- uno-backend/src/main.rs
+++ uno-backend/src/main.rs
@@ -3,29 +3,34 @@ use actix_files::Files;
 use tokio;
 use uno_frontend::{ServerApp, ServerAppProps};
 
-
+// Function to render HttpRequests (This service is called with all requests)
 #[get("/{tail:.*}")]
 async fn render_app(req: HttpRequest) -> impl Responder {
+    // Asynchronous filesystem manipulation operations that read the index.html template
     let index_html_s = tokio::fs::read_to_string("./dist/index.html")
     .await
     .expect("Failed to read index.html");
     
-
+    // Get the url from the HttpRequest
     let url = req.uri().to_string();
 
+    // Call yew::ServerRenderer using the ServerApp and Props { url: AttrValue <- virtual DOM definition }
     let renderer = yew::ServerRenderer
         ::<ServerApp>
         ::with_props(move || ServerAppProps { url: url.into(),  }
     );
     
-    let rendered = renderer.render().await;    
+    // String that return the method render()
+    let rendered = renderer.render().await;
+    // Replace the <body> of the index.html template with the string render()    
     let rendered = index_html_s.replace("<body>", &format!("<body>{}", &rendered));
 
-
+    // Response define the type and use the variable rendered as body 
     let resp = HttpResponse::Ok()
        .content_type("text/html; charset=utf-8")
        .body(rendered);
 
+    // Return the response
     return resp;
 }
 
@@ -34,9 +39,11 @@ async fn render_app(req: HttpRequest) -> impl Responder {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| 
                     App::new()
+                        // Assets/files and root directory of the render
                         .service(
                             Files::new("/dist", "./dist")
                         )
+                        // Start the service of the render app
                         .service(render_app))
         .bind(("0.0.0.0", 8080))?
         .run()
--- uno-frontend/src/lib.rs
+++ uno-frontend/src/lib.rs
@@ -9,21 +9,24 @@ pub use yew::{Properties, AttrValue};
 pub use router::{switch, Route};
 pub use components::*;
 
-
+// ServerAppProps this is used on backend for ServerRenderer props
 #[derive(Properties, PartialEq, Eq, Debug)]
 pub struct ServerAppProps {
     pub url: AttrValue,
 }
 
+// ServerApp contains the Router compatible with ServerSideRendering
 #[function_component(ServerApp)]
 pub fn server_app(props: &ServerAppProps) -> Html {
+    // A History that provides a universial API to the underlying history type.
     let history = AnyHistory::from(MemoryHistory::new());
+    // Pushes a route entry with None being the state.
     history.push(&*props.url); 
     
+    // Return the router
     html! {
         <Router history={history}>
             <Switch<Route> render={switch} />
         </Router>
     }
-
 }
