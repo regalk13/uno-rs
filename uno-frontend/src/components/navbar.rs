a072837f0e82e72bdbf05a9511efe073425566fb
Regalk <jsalazarcaviedes30@gmail.com>
2022-12-20 17:25:12 -0500

    Image support

--- uno-frontend/src/components/navbar.rs
+++ uno-frontend/src/components/navbar.rs
@@ -5,7 +5,7 @@ pub fn navbar() -> Html {
     html! {
         <nav class="p-5 bg-white shadow md:flex md:items-center md:justify-between">
             <div>
-                <img src="../assets/logo.png" alt="logo" />
+                <img src="assets/logo.png" alt="logo" />
                 <span class="text-2xl font-bold 2cursor-pointer">
                     { "Uno Rust" }
                 </span>
