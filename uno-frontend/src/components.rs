a0abd689496ea1939ea0ca38135e76c9409518ff
regalk <jsalazarcaviedes30@gmail.com>
2023-03-25 11:03:28 -0500

    Delete frontend

--- uno-frontend/src/components.rs
+++
@@ -1,9 +0,0 @@
-pub mod chat;
-pub mod login_form;
-pub mod navbar;
-pub mod register_form;
-
-pub use chat::{Chat, ChatView};
-pub use login_form::LoginForm;
-pub use navbar::NavBar;
-pub use register_form::RegisterForm;
--- uno-frontend/src/router/views/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod home;
-pub mod login;
-pub mod register;
