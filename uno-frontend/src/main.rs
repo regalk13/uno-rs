fd224934e310b039964191817d82dfdfbac0752b
regalk <jsalazarcaviedes30@gmail.com>
2023-03-26 02:12:12 -0500

    Leptos example

---
+++ uno-frontend/src/main.rs
@@ -0,0 +1,29 @@
+use leptos::*;
+
+#[component]
+pub fn SimpleCounter(cx: Scope, initial_value: i32) -> impl IntoView {
+    // create a reactive signal with the initial value
+    let (value, set_value) = create_signal(cx, initial_value);
+
+    // create event handlers for our buttons
+    // note that `value` and `set_value` are `Copy`, so it's super easy to move them into closures
+    let clear = move |_| set_value(0);
+    let decrement = move |_| set_value.update(|value| *value -= 1);
+    let increment = move |_| set_value.update(|value| *value += 1);
+
+    // create user interfaces with the declarative `view!` macro
+    view! {
+        cx,
+        <div>
+            <button on:click=clear>"Clear"</button>
+            <button on:click=decrement>"-1"</button>
+            <span>"Value: " {value} "!"</span>
+            <button on:click=increment>"+1"</button>
+        </div>
+    }
+}
+
+// Easy to use with Trunk (trunkrs.dev) or with a simple wasm-bindgen setup
+pub fn main() {
+    mount_to_body(|cx| view! { cx,  <SimpleCounter initial_value=3 /> })
+}
