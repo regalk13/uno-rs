4f548205b69c21d3d61164e868f5e6c1973c4f76
regalk <jsalazarcaviedes30@gmail.com>
2023-03-26 05:26:29 -0500

    New Structure

--- default.nix
+++
@@ -1,8 +0,0 @@
-let tools = import ./tools.nix; in with tools;
-naersk.buildPackage {
-    src = ./.;
-    cargoBuild = args: '''';
-    copyTarget = false;
-    postInstall=''trunk build -d $out'';
-    buildInputs= [trunk wasm-bindgen-cli binaryen];
-}
