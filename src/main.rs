f38ced1f3365352ca2529cf0e84e189fe33d89c1
regalk <jsalazarcaviedes30@gmail.com>
2023-03-27 00:46:46 -0500

    Init new structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use cfg_if::cfg_if;
+use leptos::*;
+
+
+cfg_if {
+   if #[cfg(feature = "ssr")]  {
+        use axum::{
+            routing::{get, post},
+            extract::Extension,
+            Router,
+        };
+        use leptos_config::get_configuration;
+        use leptos_axum::{LeptosRoutes,generate_route_list};
+        use std::sync::Arc;
+
+        #[tokio::main]
+        async fn main() {
+            let conf = get_configuration(Some("Cargo.toml")).await.unwrap();
+            let leptos_options = conf.leptos_options;
+            let addr = letops_options.site_address.clone();
+
+
+        }
+   }
+
+}
+
+else {
+
 }
