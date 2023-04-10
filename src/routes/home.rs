047cf5061b07f027da9976c3aca80ef7ae279351
regalk <jsalazarcaviedes30@gmail.com>
2023-04-09 21:55:59 -0500

    Working on the main page view

--- src/routes/home.rs
+++ src/routes/home.rs
@@ -7,6 +7,16 @@ pub fn Home(cx: Scope) -> impl IntoView {
         cx,
         <section>
             <NavBar />
+            <main class="flex">
+                <div>
+                    <h1>{"The Uno Game"}</h1>
+                    <p>{"Invite your friends and rustify the world of UNO"}</p>
+                    <input type="button" value="Play now" />
+                </div>
+                <div>
+                    <img src="main-image.png" />
+                </div>
+            </main>
         </section>
     }
 }
